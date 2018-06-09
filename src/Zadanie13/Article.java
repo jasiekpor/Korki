e6d1477926964bcf467b7fec1be40e1e86f7cfa5
jasiek <jasiekpor@gmail.com>
2018-06-09 13:20:17 +0200

    Zadanie 14 i poprawka zadania 13

--- src/Zadanie13/Article.java
+++ src/Zadanie13/Article.java
@@ -6,15 +6,13 @@ public class Article {
     double price;
     String description;
 
-    Article(int id, String name, double price, String description){
-        this.id = id;
-        this.name = name;
-        this.price = price;
-        this.description = description;
-    }
-
     public static void main(String[] args) {
-        Article article = new Article(1, "Matma dla opornych", 69.69, "Bardzo pomocna książka");
+        Article article = new Article();
+        article.id = 1;
+        article.name = "Matma dla opornych";
+        article.price = 69.69;
+        article.description = "bardzo dobry artykol";
+
         System.out.println(article.id);
         System.out.println(article.name);
         System.out.println(article.price);
---
+++ src/Zadanie14/Article.java
@@ -0,0 +1,27 @@
+package Zadanie14;
+
+public class Article {
+    static int counter;
+    int id;
+    String name;
+    double price;
+    String description;
+
+    Article(){
+        counter++;
+    }
+
+    Article(int id, String name, double price, String description){
+        this.id = id;
+        this.name = name;
+        this.price = price;
+        this.description = description;
+        counter++;
+    }
+
+    public static void main(String[] args) {
+        new Article(1, "Matma dla opornych", 69.69, "Bardzo pomocna książka");
+        new Article();
+        System.out.println(Article.counter);
+    }
+}
